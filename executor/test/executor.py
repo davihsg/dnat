3d24cc8bbd88f25d5089e1b5b063b01822318504
davihsg <daviandlion@gmail.com>
2025-11-27 00:09:05 -0300

    sync

--- executor/test/executor.py
+++ executor/test/executor.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+import os
+
 print("Hello from SGX enclave!")
+
+# Check if secret was injected by CAS
+key = os.environ.get('CODE_KEY')
+if key:
+    print(f"Secret received from CAS: {key[:20]}...")
+else:
+    print("No secret found in environment")
 
